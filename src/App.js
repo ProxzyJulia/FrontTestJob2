1c6f42edda47ca30bcc34bee6c86b88ec3754d02
JuliaNS <proxzyb@gmail.com>
2023-12-01 00:37:41 +0300

    Added button connect metaMask
    Added connection to metaMask using wagmi

--- src/App.js
+++ src/App.js
@@ -1,17 +1,75 @@
 import './App.css';
+import { WagmiConfig, createConfig, configureChains, mainnet,useConnect } from 'wagmi';
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
+import { publicProvider } from 'wagmi/providers/public'
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [mainnet],[publicProvider()]);
+
+  const config = createConfig({
+    autoConnect: true,
+    connectors: [
+      new MetaMaskConnector({ chains })
+      
+    ],
+    publicClient,
+    webSocketPublicClient,
+  })
+
+
 const collection = '0xc3f733ca98e0dad0386979eb96fb1722a1a05e69'
 const responseNft = await fetch(
   `https://api.reservoir.tools/tokens/v4?collection=${collection}&limit=1`
 );
+
+
+
+
+
+
 var objNFT = []
 const tokId=0
 if (responseNft.status === 200)
 {
   objNFT = await responseNft.json();
 }
+
+
+ 
+export function Profile() {
+  const { connect, connectors, error, isLoading, pendingConnector } =
+    useConnect()
+ 
+  return (
+    <div>
+      {connectors.map((connector) => (
+        <button
+          disabled={!connector.ready}
+          key={connector.id}
+          onClick={() => connect({ connector })}
+        >
+          {connector.name}
+          {!connector.ready && ' (unsupported)'}
+          {isLoading &&
+            connector.id === pendingConnector?.id &&
+            ' (connecting)'}
+        </button>
+      ))}
+ 
+      {error && <div>{error.message}</div>}
+    </div>
+  )
+}
+
+
 function App() {
   return (
-    <><img src={objNFT.tokens[tokId].image} /><table>
+    
+    <><WagmiConfig config={config}>
+
+      <Profile />
+
+    </WagmiConfig><><img src={objNFT.tokens[tokId].image} /><table>
       <tr>
         <td>NFT id</td><td>{objNFT.tokens[tokId].tokenId}</td>
       </tr>
@@ -33,7 +91,7 @@ function App() {
       <tr>
         <td>Price</td><td>{objNFT.tokens[tokId].floorAskPrice}</td>
       </tr>
-    </table></>
+    </table></></>
     
   );
 }
